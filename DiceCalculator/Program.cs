7b92234597f466bea3b8e67122e021f052f1d398
Aurora Dawn <121758425+StellarWitch642@users.noreply.github.com>
2023-04-21 16:18:55 -0400

    Initial commit

---
+++ DiceCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DiceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool appRunning = true;
+
+            while (appRunning)
+            {
+                Console.WriteLine("Enter the amount and type of die. Example: 6d10");
+                string input = Console.ReadLine();
+
+                //Exit program
+                if (Console.ReadLine() == "exit")
+                {
+                    appRunning = false;
+                }
+            }
+        }
+    }
+}
